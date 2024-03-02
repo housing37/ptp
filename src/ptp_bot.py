aa74f8e94eb7c33f02423e5faf3eec622673566a
house <housesellout@gmail.com>
2024-03-02 16:28:01 -0500

    :- ptp_bot - commented out ‘filter’ command aspect 	- trying to get rid of erroneous error logging 	- seems to be working, but not sure what i disabled 	- gen_image still seems to work just fine

--- src/ptp_bot.py
+++ src/ptp_bot.py
@@ -425,7 +425,7 @@ def main():
 
     # Register message handler for all other messages
     # dp.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo)) # ~ = negate (ie. AND NOT)
-    dp.add_handler(MessageHandler(filters.Command, bad_command))
+    # dp.add_handler(MessageHandler(filters.Command, bad_command))
     # Start the Bot
     dp.run_polling()
     # Update.start_polling()
